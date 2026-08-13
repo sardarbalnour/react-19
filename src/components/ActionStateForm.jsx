022529de58fbff759590e78e7a8fdec23a906805
Sardar balnour <balnour.sardar@gmail.com>
2026-08-13 23:02:25 +0330

    mutate data

--- src/components/ActionStateForm.jsx
+++ src/components/ActionStateForm.jsx
@@ -1,9 +1,30 @@
 import { useActionState } from "react";
 
+import { BASE_URL } from "../constants/constants";
+
 function ActionStateForm() {
-  const createPost = (previousState, formData) => {
+  const createPost = async (previousState, formData) => {
     const form = Object.fromEntries(formData.entries());
-    console.log({ previousState, form });
+    // console.log({ previousState, form });
+
+    try {
+      const response = await fetch(`${BASE_URL}/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Request failed : ${response.status}`);
+      }
+
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.log(err.message);
+    }
   };
 
   const [state, submitAction, isPending] = useActionState(createPost, null);
@@ -16,6 +37,7 @@ function ActionStateForm() {
 
         <button type="submit">Submit</button>
       </form>
+      {!!state && <pre>{JSON.stringify(state, null, 2)}</pre>}
     </div>
   );
 }
